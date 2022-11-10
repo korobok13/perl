56c3fe41f0088a4f9afb453b61f7ce2f8d662402
korobov_o <korobok13@yandex.ru>
2022-11-10 16:46:44 +0300

    rand

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 31;
-int numberB = 7;
+﻿int numberA = new Random().Next(1, 99); // 1 2 3 4 .....98
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 99);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
 
